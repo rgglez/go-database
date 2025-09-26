972b6a5e31ca80746cb240776db108e26b67cd48
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2025-09-25 19:42:25 -0600

    Fix

--- mysql.go
+++ mysql.go
@@ -14,7 +14,7 @@ import (
 //-----------------------------------------------------------------------------
 
 // Specific MySQL configuration options, "extending" the base options
-type ConfigMySQL struct {
+type MySQL struct {
 	BaseDatabase
 	TimeZone    string
 	MaxOpenConn int
@@ -25,19 +25,13 @@ type ConfigMySQL struct {
 
 //-----------------------------------------------------------------------------
 
-type MySQL struct{
-	Config ConfigMySQL
-}
-
-//-----------------------------------------------------------------------------
-
 // Factory
 type MySQLFactory struct{}
 
 //-----------------------------------------------------------------------------
 
 // Convenience function, if we want to create a MySQL instance directly
-func NewMySQL(mysql ConfigMySQL) (Database, error) {
+func NewMySQL(mysql MySQL) (Database, error) {
 	return NewDatabase("mysql", mysql)
 }
 
@@ -52,20 +46,20 @@ func (f MySQLFactory) Create(database interface{}) (Database, error) {
 	}
 
 	// Set defaults if needed
-	if mysql.Config.TimeZone == "" {
-		mysql.Config.TimeZone = "America/New_York"
+	if mysql.TimeZone == "" {
+		mysql.TimeZone = "America/New_York"
 	}
-	if mysql.Config.MaxOpenConn == 0 {
-		mysql.Config.MaxOpenConn = 1000
+	if mysql.MaxOpenConn == 0 {
+		mysql.MaxOpenConn = 1000
 	}
-	if mysql.Config.MaxIdleConn == 0 {
-		mysql.Config.MaxIdleConn = 10
+	if mysql.MaxIdleConn == 0 {
+		mysql.MaxIdleConn = 10
 	}
-	if mysql.Config.MaxLifeTime == 0 {
-		mysql.Config.MaxLifeTime = 60
+	if mysql.MaxLifeTime == 0 {
+		mysql.MaxLifeTime = 60
 	}
-	if mysql.Config.MaxIdleTime == 0 {
-		mysql.Config.MaxIdleTime = 60
+	if mysql.MaxIdleTime == 0 {
+		mysql.MaxIdleTime = 60
 	}
 
 	return mysql, nil
@@ -77,7 +71,7 @@ func (f MySQLFactory) Create(database interface{}) (Database, error) {
 func (d MySQL) GetConnection() (*gorm.DB, error) {
 	// Get the cache connection key from the DSN
 	hasher := sha1.New()
-	hasher.Write([]byte(d.Config.Dsn))
+	hasher.Write([]byte(d.Dsn))
 	hash := hasher.Sum(nil)
 	key := hex.EncodeToString(hash)
 
@@ -88,19 +82,19 @@ func (d MySQL) GetConnection() (*gorm.DB, error) {
 
 	// Set the logger if injected
 	var gormConfig *gorm.Config = nil
-	if d.Config.Logger != nil {
+	if d.Logger != nil {
 		gormConfig = &gorm.Config{
-			Logger: d.Config.Logger,
+			Logger: d.Logger,
 		}
 	}
 
-	conn, err := gorm.Open(mysql.Open(d.Config.Dsn), gormConfig)
+	conn, err := gorm.Open(mysql.Open(d.Dsn), gormConfig)
 	if err != nil {
 		return nil, err
 	}
 	// Set the database cache (gormcache) if injected
-	if d.Config.Cache != nil {
-		conn.Use(d.Config.Cache)
+	if d.Cache != nil {
+		conn.Use(d.Cache)
 	}
 
 	// Configure the connection pool
@@ -110,18 +104,18 @@ func (d MySQL) GetConnection() (*gorm.DB, error) {
 	}
 
 	// Set timezone
-	tzSQL := fmt.Sprintf("SET time_zone='%s';", d.Config.TimeZone)
+	tzSQL := fmt.Sprintf("SET time_zone='%s';", d.TimeZone)
 	conn.Exec(tzSQL)
-	if d.Config.Debug {
+	if d.Debug {
 		var sessionTimeZone string
 		conn.Raw("SELECT @@session.time_zone").Scan(&sessionTimeZone)
 		log.Println("Database timezone:", sessionTimeZone)
 	}
 
-	sqlDB.SetMaxOpenConns(d.Config.MaxOpenConn)
-	sqlDB.SetMaxIdleConns(d.Config.MaxIdleConn)
-	sqlDB.SetConnMaxLifetime(d.Config.MaxLifeTime * time.Minute)
-	sqlDB.SetConnMaxIdleTime(d.Config.MaxIdleTime * time.Minute)
+	sqlDB.SetMaxOpenConns(d.MaxOpenConn)
+	sqlDB.SetMaxIdleConns(d.MaxIdleConn)
+	sqlDB.SetConnMaxLifetime(d.MaxLifeTime * time.Minute)
+	sqlDB.SetConnMaxIdleTime(d.MaxIdleTime * time.Minute)
 
 	DatabaseCache.Store(key, conn)
 
